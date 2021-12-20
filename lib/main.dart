711554ba67eb0ee60e8d3f06df8e3bf0f6a69af9
Dumrong <dumdumrong13@gmail.com>
2021-12-20 16:19:11 +0700

    Update v1

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,15 @@ void main() {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("เทคโนโลยีสารสนเทศ"),
+          title: Text(
+            "เทคโนโลยีสารสนเทศ",
+            style: TextStyle(fontFamily: "Sarabun", fontSize: 25),
+          ),
           centerTitle: true,
           backgroundColor: Colors.deepPurple,
         ),
         body: Center(
-          child: Text("นักพัฒนาซอฟแวร์"),
+          child: Image.asset("assets/images/bird.jpg")
         ),
         floatingActionButton: FloatingActionButton(
           child: Text("+"),
@@ -24,7 +27,8 @@ void main() {
         bottomNavigationBar: BottomAppBar(
           child: Container(
             child: Center(
-              child: Text("วิทยาลัยเทคนิคตราด",style: TextStyle(color: Colors.white)),
+              child: Text("วิทยาลัยเทคนิคตราด",
+                  style: TextStyle(color: Colors.white,fontFamily: "Sarabun", fontSize: 18)),
             ),
             height: 40,
           ),
